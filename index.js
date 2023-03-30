dae118eacc1ce623a15505b114caa5cf62b84300
Kerim Kaan Dönmez <kerimkaan@bisu.com.tr>
2023-03-31 01:32:28 +0300

    chore: linting

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const {
-	DynamoDBDocumentClient,
-	ScanCommand,
+    DynamoDBDocumentClient,
+    ScanCommand,
+    PutCommand
 } = require("@aws-sdk/lib-dynamodb");
 const axios = require("axios");
 
@@ -17,36 +18,36 @@ const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3/coins/list';
  * @returns An array of objects.
  */
 const getCoinsList = async () => {
-	try {
-		const { data } = await axios.get(COINGECKO_API_URL);
-		return data;
-	} catch (error) {
-		console.log(error);
-		return [];
-	}
+    try {
+        const { data } = await axios.get(COINGECKO_API_URL);
+        return data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
 };
 
 exports.handler = async (event) => {
-	console.log("EVENT: ", event);
-	try {
-		let result;
+    console.log("EVENT: ", event);
+    try {
+        let result;
         const params = {
-			TableName: COIN_TABLE
-		};
-		// Scan the table to see if there is any data
-		const { Items } = await docClient.send(new ScanCommand(params));
-		if (Items.length > 0) {
-			// If there is data in the database, we return it
-			result = {
-				success: true,
-				coins: Items,
-			};
-		} else {
-			const promises = [];
-			// If there is no data in the database, we get it from the API
-			// Get the coins list from Coingecko API
-			const getFromApi = await getCoinsList();
-			/*  if (getFromApi.length > 0) {
+            TableName: COIN_TABLE,
+        };
+        // Scan the table to see if there is any data
+        const { Items } = await docClient.send(new ScanCommand(params));
+        if (Items.length > 0) {
+            // If there is data in the database, we return it
+            result = {
+                success: true,
+                coins: Items,
+            };
+        } else {
+            const promises = [];
+            // If there is no data in the database, we get it from the API
+            // Get the coins list from Coingecko API
+            const getFromApi = await getCoinsList();
+            /*  if (getFromApi.length > 0) {
                 await docClient.send(new BatchWriteCommand(
                     {
                         RequestItems: {
@@ -62,39 +63,39 @@ exports.handler = async (event) => {
                     }}
                 ));
             } */
-			if (getFromApi.length > 0) {
-				// If we get data from the API, we save it in the database
-				for (const coin of getFromApi) {
-					const params = {
-						TableName: COIN_TABLE,
-						Item: {
-							id: coin.id,
-							symbol: coin.symbol,
-							name: coin.name,
-						},
-					};
-					promises.push(docClient.send(new PutCommand(params)));
-				}
-				await Promise.all(promises);
-			}
-			result = {
-				success: true,
-				coins: getFromApi,
-			};
-		}
-		return {
-			statusCode: 200,
-			body: JSON.stringify(result),
-		};
-	} catch (error) {
-		console.log(error);
-		return {
-			statusCode: 500,
-			body: JSON.stringify({
-				success: false,
-				message: "Internal Server Error",
-				error,
-			}),
-		};
-	}
+            if (getFromApi.length > 0) {
+                // If we get data from the API, we save it in the database
+                for (const coin of getFromApi) {
+                    const params = {
+                        TableName: COIN_TABLE,
+                        Item: {
+                            id: coin.id,
+                            symbol: coin.symbol,
+                            name: coin.name,
+                        },
+                    };
+                    promises.push(docClient.send(new PutCommand(params)));
+                }
+                await Promise.all(promises);
+            }
+            result = {
+                success: true,
+                coins: getFromApi,
+            };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(result),
+        };
+    } catch (error) {
+        console.log(error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                success: false,
+                message: "Internal Server Error",
+                error,
+            }),
+        };
+    }
 };
